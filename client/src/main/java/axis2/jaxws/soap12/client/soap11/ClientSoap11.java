ab4692346235165011f27d8ecaad9a0fc415c9c8
emag <lanabe.lanabe@gmail.com>
2016-06-17 17:26:13 +0900

    Read the generated wsdl

--- client/src/main/java/axis2/jaxws/soap12/client/soap11/ClientSoap11.java
+++ client/src/main/java/axis2/jaxws/soap12/client/soap11/ClientSoap11.java
@@ -1,9 +1,19 @@
 package axis2.jaxws.soap12.client.soap11;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ClientSoap11 {
 
-    public String invokeSayHello(String text) {
-        HelloWorldSoap11_Service service = new HelloWorldSoap11_Service();
+    public String invokeSayHello(String text) throws MalformedURLException {
+        HelloWorldSoap11_Service service;
+
+        String wsdlLocation = System.getProperty("soap11.wsdl.location");
+        if (wsdlLocation != null) {
+            service = new HelloWorldSoap11_Service(new URL("file://" + wsdlLocation));
+        } else {
+            service = new HelloWorldSoap11_Service();
+        }
         HelloWorldSoap11 stub = service.getHelloWorldSoap11Port();
         return stub.sayHello(text);
     }
--- client/src/main/java/axis2/jaxws/soap12/client/soap12/ClientSoap12.java
+++ client/src/main/java/axis2/jaxws/soap12/client/soap12/ClientSoap12.java
@@ -1,9 +1,19 @@
 package axis2.jaxws.soap12.client.soap12;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ClientSoap12 {
 
-    public String invokeSayHello(String text) {
-        HelloWorldSoap12_Service service = new HelloWorldSoap12_Service();
+    public String invokeSayHello(String text) throws MalformedURLException {
+        HelloWorldSoap12_Service service;
+
+        String wsdlLocation = System.getProperty("soap12.wsdl.location");
+        if (wsdlLocation != null) {
+            service = new HelloWorldSoap12_Service(new URL("file://" + wsdlLocation));
+        } else {
+            service = new HelloWorldSoap12_Service();
+        }
         HelloWorldSoap12 stub = service.getHelloWorldSoap12Port();
         return stub.sayHello(text);
     }
